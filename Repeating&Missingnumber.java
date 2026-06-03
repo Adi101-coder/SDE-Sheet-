7a341e94e3b0b4bdbae666b0cf56633ca7b5e1f3
Adit <aditkatiyar050@gmail.com>
2026-06-04 05:18:06 +0530

    week 4 completed

---
+++ Repeating&Missingnumber.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int[] findErrorNums(int[] nums) {
+        int arr[]=new int[2];
+        int freq[]=new int[nums.length+1];
+        for(int i=0;i<nums.length;i++)  freq[nums[i]]++;
+        for(int i=1;i<=nums.length;i++)
+        {
+            if(freq[i]==2)  arr[0]=i;
+            else if(freq[i]==0) arr[1]=i;
+        }
+        return arr;
+    }
+
+}
---
+++ Week 4/Count_inversion.java
@@ -0,0 +1,16 @@
+// We have the unorthodox way as
+// class Solution {
+//     public long numberOfInversions(int[] nums) {
+//         int cnt=0;
+//         for(int i=0;i<nums.length;i++)
+//         {
+//             for(int j=i+1;j<nums.length;j++)
+//             {
+//                     if(nums[i]>nums[j]) cnt++;   
+//             }
+//         }
+//         return cnt;
+//     }
+// }
+
+
